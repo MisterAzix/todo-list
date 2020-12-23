51e154b31675b8fdf63afe51634d50a87e3ec3df
MisterAzix <flizi@live.fr>
2020-12-23 21:03:36 +0100

    First commit : Project Struct & First page front

---
+++ form.php
@@ -0,0 +1,13 @@
+<form action="" method="get" class="form-container">
+    <div class="form-subcontainer">
+        <label for="email">Email Address *</label>
+        <input type="email" name="email" id="email" placeholder="contact@example.com" required>
+    </div>
+    <div class="form-subcontainer">
+        <label for="password">Password *</label>
+        <input type="password" name="password" id="password" placeholder="••••••••••••••••••••" required>
+    </div>
+    <div class="form-submit-container">
+        <input type="submit" value="Log in">
+    </div>
+</form>
---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>ToDo List</title>
+    <meta name="description" content="Just a simple exercice in PHP !">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+    <h1>Welcome, please log in to access your ToDo list !</h1>
+    <?php require 'form.php'; ?>
+</body>
+
+</html>
